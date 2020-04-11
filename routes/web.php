2c0e9356770f86baf406da6bbe188ba44a0f6642
zgj12306 <2331766479@qq.com>
2020-04-11 13:28:11 +0000

    用户注册表单

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/login', 'StaticPagesController@login')->name('login');
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
