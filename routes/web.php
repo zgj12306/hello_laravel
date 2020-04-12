c029b1efd48a64ea917002cb137382801e5c7fcc
zgj12306 <2331766479@qq.com>
2020-04-12 11:30:00 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('/login', 'StaticPagesController@login')->name('login');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
