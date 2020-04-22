499e098c9572474e88b3a3ecddcbb7a94f675622
zgj12306 <2331766479@qq.com>
2020-04-22 00:44:57 +0000

    关注和粉丝列表页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,12 +65,12 @@ class User extends Authenticatable
 
     public function followers()
     {
-        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
 
     public function followings()
     {
-        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
     }
 
     public function follow($user_ids)
