f48efcb0c79cd9c474df38c7b19f4286738a5112
zgj12306 <2331766479@qq.com>
2020-05-02 09:24:11 +0000

    修改一些bug

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo APP') - Laravle 新手入门教程</title>
+    <title>@yield('title', 'Weibo APP') - Laravle 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}"></script>
 </head>
